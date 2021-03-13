fa813fdeb7c9a3460f11903af50f42d507841fca
marcelomarinss <57117356+marcelomarinss@users.noreply.github.com>
2021-03-13 10:14:06 -0300

    Add files via upload

---
+++ JS/action.js
@@ -0,0 +1,47 @@
+
+const calcular = document.getElementById('calcular');
+const limpar = document.getElementById('limpar');
+
+    function imc() {
+        const nome = document.getElementById('nome').value;
+        const altura = document.getElementById('altura').value;
+        const peso = document.getElementById('peso').value;
+        const resultado = document.getElementById('resultado');
+        const valorIMC = (peso / (altura * altura)).toFixed(2);
+
+        if(nome !== '' && altura !== '' && peso !== '') {
+            
+            let classificacao = "";
+
+            if(valorIMC < 18.5){
+                classificacao = 'abaixo do peso.'
+            
+            } else if (valorIMC < 25) {
+                classificacao = 'no seu peso ideal. Parabéns!';
+            } else if (valorIMC < 30){
+                classificacao = 'acima do peso.';
+            } else if (valorIMC < 35){
+                classificacao = 'com obesidade grau I.';
+            } else if (valorIMC < 40){
+                classificacao = 'com obesidade grau II.';
+            } else {
+                classificacao = 'com obesidade grau III. CUIDADO!';
+            }
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`
+            
+        } else {
+            resultado.textContent = 'Preencha todos os campos!!';
+        }
+
+        
+    }
+
+    function limpa(){
+
+        if(resultado.textContent !== '') {
+            resultado.textContent = '';
+        }
+    }
+
+calcular.addEventListener ('click', imc);
+limpar.addEventListener('click', limpa);
